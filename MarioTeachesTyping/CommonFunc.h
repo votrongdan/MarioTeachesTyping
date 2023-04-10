fc3c11c70bd701cfa409f26e80807fadcdd16cb5
Vo Trong Dan <trongdantvh@gmail.com>
2023-04-10 22:17:18 +0700

    remake main.cpp,  add CommonFunc.h and CommonFunc.cpp

---
+++ MarioTeachesTyping/CommonFunc.h
@@ -0,0 +1,15 @@
+#ifndef COMMON_FUNC_H
+#define COMMON_FUNC_H
+
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_Image.h>
+#include <string>
+
+// screen dimention constants
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+
+
+#endif
